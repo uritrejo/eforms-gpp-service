826ecb80f6fe518c56b13133e3b83e830235f3da
uritrejo <uritrejo20@gmail.com>
2025-06-02 22:34:14 +0200

    added cors mappings (* for now)

---
+++ src/main/java/it/polimi/gpplib/eforms_gpp_service/config/WebConfig.java
@@ -0,0 +1,24 @@
+package it.polimi.gpplib.eforms_gpp_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        // TODO: update this with more secure setttings
+                        // .allowedOrigins("http://some-url.com", "https://another-url.com")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
