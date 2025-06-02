09ce1d7cc0d944897a41eef9c6a011660f605a35
uritrejo <uritrejo20@gmail.com>
2025-06-02 13:07:53 +0200

    setup project

---
+++ src/main/java/it/polimi/gpplib/eforms_gpp_service/EformsGppServiceApplication.java
@@ -0,0 +1,13 @@
+package it.polimi.gpplib.eforms_gpp_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EformsGppServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EformsGppServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/polimi/gpplib/eforms_gpp_service/EformsGppServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.polimi.gpplib.eforms_gpp_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EformsGppServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
